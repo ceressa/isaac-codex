4794a1a975e4e486d45ba2a00c992e40487bc2b8
ceressa <ceressa@users.noreply.github.com>
2026-07-22 17:32:29 +0300

    Deploy: blood-drop favicon

---
+++ flutter_service_worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function (event) { self.skipWaiting(); });
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (keys) {
+        return Promise.all(keys.map(function (key) { return caches.delete(key); }));
+      })
+      .then(function () { return self.clients.claim(); })
+  );
+});
